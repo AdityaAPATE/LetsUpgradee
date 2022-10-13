cce6430e28aab69200d3d27867d85ebc93e112a1
AdityaAPATE <115172879+AdityaAPATE@users.noreply.github.com>
2022-10-13 23:04:27 +0530

    Assignment no 1

---
+++ GodisLove(1).js
@@ -0,0 +1,8 @@
+var user1 = prompt("Enter  A number : ")
+var user2 = prompt ("Enter B Number : ")
+if (user1 > user2){
+    document.write("A number is greater than B")
+}
+else{
+    document.write("A number is less than B")
+}
